bd389a19b6459779b05ddf29e789d6f6085b264e
Kyle <kylejordanwilliams@gmail.com>
2023-04-20 12:34:09 +0100

    Updated push

--- src/components/AddItem/index.test.js
+++ src/components/AddItem/index.test.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 // Check if the user input appear in the input box on the browser
-// Import stuff from jest 
+// Import stuff from jest.
 import { test, expect } from "@jest/globals";
 // Import stuff from React testing library
 import { render, screen } from "@testing-library/react";
@@ -13,28 +13,25 @@ import userEvent from "@testing-library/user-event";
 //      AddItem
 import AddItem from "./index.js";
 
-
 //- Write a test that verifies that the `AddItem` component shows the user:
 //- an input with an "Add item:" label
 //- a button containing the value of the `buttonText` prop
 
-test("Input should have 'Add item' label", function() {
-// 3 As:
-// Arrange
-//  render the component
-render(<AddItem />);
+test("Input should have 'Add item' label", function () {
+  // 3 As:
+  // Arrange
+  //  render the component
+  render(<AddItem />);
 
-// screen.logTestingPlaygroundURL();
-// screen.debug();
+  // screen.logTestingPlaygroundURL();
+  // screen.debug();
 
+  // Act
+  //  Make a variable to assign the input that we want to check
+  const actual = screen.getByLabelText(/Add item:/i);
+  const expected = "Add item:";
 
-// Act 
-//  Make a variable to assign the input that we want to check
-const actual = screen.getByLabelText(/Add item:/i);
-const expected = "Add item:";
-
-// Assert
-//  Compare actual and expected to check if the label of the input has value of 'AddItem:'
-expect(actual).toHaveTextContent(expected);
-
-})
+  // Assert
+  //  Compare actual and expected to check if the label of the input has value of 'AddItem:'
+  expect(actual).toHaveTextContent(expected);
+});
